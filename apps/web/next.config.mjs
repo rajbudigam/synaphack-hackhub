b0dd1d9f4c464ba9e1e35b0dd4d1f8484f27babb
rajbudigam <163419369+rajbudigam@users.noreply.github.com>
2025-08-20 15:13:01 +0530

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Optional: if lint plugins ever fail CI, uncomment next line
-  // eslint: { ignoreDuringBuilds: true },
 };
-
 export default nextConfig;
